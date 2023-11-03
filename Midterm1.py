f884f360fc78bb45c0334f54b5adc84c6ea7210e
Patrick Lynch <143531877+XJrain@users.noreply.github.com>
2023-11-02 21:24:05 -0400

    Add files via upload

--- Midterm1.py
+++ Midterm1.py
@@ -58,6 +58,11 @@ def extract_data_from_row(row_div):
 for row_div in main_data_rows:
     extracted_data = extract_data_from_row(row_div)
     if extracted_data:
+        login_id, repo_count, follower_count, member_since = extracted_data
+
+        if follower_count < 0 or member_since == "0000-00-00":
+            continue
+
         data.append(extracted_data)
 
 # Convert the data list to a DataFrame
@@ -69,3 +74,60 @@ df = df[df["Login ID"].notna() & (df["Login ID"] != "")]
 
 # Save the DataFrame to a CSV file
 df.to_csv("github_users.csv", index=False)
+
+# Part 2
+
+#Import the time module
+import time
+
+with open('token.txt', 'r') as f:
+    TOKEN = f.read().strip()
+
+API_URL = "https://api.github.com/users/"
+login_ids = df["Login ID"].tolist()
+
+headers = {
+    'Authorization': f'token {TOKEN}',
+    'Accept': 'application/vnd.github.v3+json'
+}
+
+data = []
+failed_logins = []
+
+print("Starting data fetching from GitHub API")
+for login_id in login_ids:
+    response = requests.get(API_URL + login_id, headers=headers)
+    if response.status_code != 200:
+        print(f"Failed to fetch data for {login_id}. Status code: {response.status_code}")
+        failed_logins.append(login_id)
+        continue
+
+    user_data = response.json()
+    avatar_url = user_data.get('avatar_url', '')
+    url = user_data.get('url', '')
+    following = user_data.get('following', '')
+    starred_response = requests.get(API_URL + login_id + '/starred', headers=headers)
+    starred_count = len(starred_response.json())
+    full_name = user_data.get('name', '')
+    company = user_data.get('company', '')
+    blog = user_data.get('blog', '')
+    location = user_data.get('location', '')
+    email = user_data.get('email', '')
+    hireable = user_data.get('hireable', '')
+    bio = user_data.get('bio', '')
+    created_at = user_data.get('created_at', '')
+    updated_at = user_data.get('updated_at', '')
+    data.append([login_id, avatar_url, url, following, starred_count, full_name, company, blog, location, email, hireable, bio, created_at, updated_at])
+
+# Filter out the failed logins from the original DataFrame
+df = df[~df["Login ID"].isin(failed_logins)]
+
+# Overwrite the original CSV file with the updated DataFrame
+df.to_csv("github_users.csv", index=False)
+
+time.sleep(1)
+
+columns = ["Login ID", "Avatar URL", "URL", "Following", "Starred Count", "Full Name", "Company", "Blog", "Location", "Email", "Hireable", "Bio", "Created At", "Updated At"]
+df_results = pd.DataFrame(data, columns=columns)
+df_results.to_csv("github_user_details.csv", index=False)
+
